b7a59b954c1af2fb0e4108a42535a62ca913f703
unknown <r0706810@student.thomasmore.be>
2020-01-22 11:43:19 +0100

    hopelijk problemen met git opgelost

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,19 @@ import {CreateVerenigingComponent} from './vereniging/create-vereniging/create-v
 import {ManageGebruikersComponent} from './admin/manage-gebruikers/manage-gebruikers.component';
 import {DetailGebruikerComponent} from './admin/detail-gebruiker/detail-gebruiker.component';
 import {CreateGebruikerComponent} from './admin/create-gebruiker/create-gebruiker.component';
+import {AdminHomeComponent} from "./admin/admin-home/admin-home.component";
 import { CreateVerantwoordelijkeComponent } from './vereniging/create-verantwoordelijke/create-verantwoordelijke.component';
+import {ManageEvenementenComponent} from "./admin/manage-evenementen/manage-evenementen.component";
+import {CreateEvenementComponent} from "./admin/create-evenement/create-evenement.component";
+import {EditEvenementComponent} from "./admin/edit-evenement/edit-evenement.component";
 
 const routes: Routes = [
   {path: 'manageGebruikers', component: ManageGebruikersComponent},
   {path: 'createGebruiker', component: CreateGebruikerComponent},
+  {path: 'adminHome', component: AdminHomeComponent},
+  {path: 'manageEvenementen', component: ManageEvenementenComponent},
+  {path: 'createEvenement', component: CreateEvenementComponent},
+  {path: 'editEvenement', component: EditEvenementComponent},
   {path: 'detailGebruiker', component: DetailGebruikerComponent},
   {path: 'createvereniging', component: CreateVerenigingComponent},
   {path: 'createverantwoordelijke', component: CreateVerantwoordelijkeComponent}

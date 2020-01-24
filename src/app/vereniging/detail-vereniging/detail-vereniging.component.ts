ff43be2bcba8c459922684b621996743599df535
Ali Kiyak <alikiyak@outlook.com>
2020-01-24 11:39:40 +0100

    detail vereniging

---
+++ src/app/vereniging/detail-vereniging/detail-vereniging.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-vereniging',
+  templateUrl: './detail-vereniging.component.html',
+  styleUrls: ['./detail-vereniging.component.scss']
+})
+export class DetailVerenigingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

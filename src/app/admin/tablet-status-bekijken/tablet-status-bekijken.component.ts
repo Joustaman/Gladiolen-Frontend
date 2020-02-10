2359dc06723ac3af753631138fa0f1ce9855f43a
Robben P <robben.praet@gmail.com>
2020-02-10 11:24:13 +0100

    opkuisen

--- src/app/admin/tablet-status-bekijken/tablet-status-bekijken.component.ts
+++ src/app/admin/tablet-status-bekijken/tablet-status-bekijken.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AdminService } from '../admin.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AdminService } from "../admin.service";
 
 @Component({
-  selector: 'app-tablet-status-bekijken',
-  templateUrl: './tablet-status-bekijken.component.html',
-  styleUrls: ['./tablet-status-bekijken.component.scss']
+  selector: "app-tablet-status-bekijken",
+  templateUrl: "./tablet-status-bekijken.component.html",
+  styleUrls: ["./tablet-status-bekijken.component.scss"]
 })
 export class TabletStatusBekijkenComponent implements OnInit {
-
   tablets: any[];
   constructor(
     private route: ActivatedRoute,
@@ -18,29 +17,27 @@ export class TabletStatusBekijkenComponent implements OnInit {
 
   ngOnInit() {
     this.tablets = this.route.snapshot.data["tablets"];
-    const now = new Date()
-    const tijd2minGeleden = new Date(now.valueOf() - 2*60000);
-    const tijdhalfuurGeleden = new Date(now.valueOf() - 30*60000);
+    const now = new Date();
+    const tijd2minGeleden = new Date(now.valueOf() - 2 * 60000);
+    const tijdhalfuurGeleden = new Date(now.valueOf() - 30 * 60000);
 
     this.tablets.forEach(tablet => {
-      if(tablet.batterijpercentage<30){
-        tablet.batterijKleur = "red"
-      }else if(tablet.batterijpercentage<70){
-        tablet.batterijKleur = "orange"
-      }else{
-        tablet.batterijKleur = "green"
+      if (tablet.batterijpercentage < 30) {
+        tablet.batterijKleur = "red";
+      } else if (tablet.batterijpercentage < 70) {
+        tablet.batterijKleur = "orange";
+      } else {
+        tablet.batterijKleur = "green";
       }
 
       let datum = new Date(tablet.laatstGeupdatet);
-      if(tijd2minGeleden.valueOf()<datum.valueOf()){
+      if (tijd2minGeleden.valueOf() < datum.valueOf()) {
         tablet.tijdKleur = "green";
-      }else if(tijdhalfuurGeleden.valueOf()<datum.valueOf()){
+      } else if (tijdhalfuurGeleden.valueOf() < datum.valueOf()) {
         tablet.tijdKleur = "orange";
-      }else{
+      } else {
         tablet.tijdKleur = "red";
       }
-      
-    });    
+    });
   }
-
 }

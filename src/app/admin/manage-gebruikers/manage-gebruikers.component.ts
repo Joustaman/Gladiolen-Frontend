bc30199e93c9772fc07cef85185abfa6f497f702
Robben P <robben.praet@gmail.com>
2020-02-17 11:32:15 +0100

    SPECIAAL VOOR ALI: excel importeren fix

--- src/app/admin/manage-gebruikers/manage-gebruikers.component.ts
+++ src/app/admin/manage-gebruikers/manage-gebruikers.component.ts
@@ -1,61 +1,59 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {AdminService} from '../admin.service';
-import { HotTableRegisterer } from '@handsontable/angular';
-import * as Handsontable from 'handsontable';
-import 'handsontable/languages/nl-NL';
-import * as XLSX from 'xlsx';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AdminService } from "../admin.service";
+import { HotTableRegisterer } from "@handsontable/angular";
+import * as Handsontable from "handsontable";
+import "handsontable/languages/nl-NL";
+import * as XLSX from "xlsx";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-manage-gebruikers',
-  templateUrl: './manage-gebruikers.component.html',
-  styleUrls: ['./manage-gebruikers.component.scss']
+  selector: "app-manage-gebruikers",
+  templateUrl: "./manage-gebruikers.component.html",
+  styleUrls: ["./manage-gebruikers.component.scss"]
 })
 export class ManageGebruikersComponent implements OnInit {
-
   gebruikers: any = [];
   pageLoaded = false;
-  str="";
+  str = "";
   private hotRegisterer = new HotTableRegisterer();
-  id = 'hotInstance';
+  id = "hotInstance";
   data: any = [];
   colHeaders = [
-    'Naam',
-    'Voornaam',
-    'Geboortedatum',
-    'Rol',
-    'Email',
-    'Telefoon',
-    'Opmerking',
-    'Rijksregisternummer',
-    'Actief',
-  ]
+    "Naam",
+    "Voornaam",
+    "Geboortedatum",
+    "Rol",
+    "Email",
+    "Telefoon",
+    "Opmerking",
+    "Rijksregisternummer",
+    "Actief"
+  ];
   excelModus = false;
   columns: any = [
-    { data: 'naam', readOnly: true },
-    { data: 'voornaam', readOnly: true },
-    { data: 'geboortedatum', readOnly: true },
-    { data: 'rol', readOnly: true},
-    { data: 'email', readOnly: true },
-    { data: 'telefoon', readOnly: true },
-    { data: 'opmerking', readOnly: true },
-    { data: 'rijksregisternummer', readOnly: true },
-    { data: 'actief', readOnly: true },
+    { data: "naam", readOnly: true },
+    { data: "voornaam", readOnly: true },
+    { data: "geboortedatum", readOnly: true },
+    { data: "rol", readOnly: true },
+    { data: "email", readOnly: true },
+    { data: "telefoon", readOnly: true },
+    { data: "opmerking", readOnly: true },
+    { data: "rijksregisternummer", readOnly: true },
+    { data: "actief", readOnly: true }
   ];
-  constructor(private adminService: AdminService,
-              private toastr: ToastrService) {
-  }
+  constructor(
+    private adminService: AdminService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
-    this.adminService.getGebruikers().subscribe(
-      result => {
-        console.log(result);
-        this.gebruikers = result;
-        this.createDataForTable(result);
-        this.pageLoaded = true;
-      },
-    );
+    this.adminService.getGebruikers().subscribe(result => {
+      console.log(result);
+      this.gebruikers = result;
+      this.createDataForTable(result);
+      this.pageLoaded = true;
+    });
   }
 
   createDataForTable(apiData: any) {
@@ -69,7 +67,7 @@ export class ManageGebruikersComponent implements OnInit {
         telefoon: gebruiker.telefoon,
         opmerking: gebruiker.opmerking,
         rijksregisternummer: gebruiker.rijksregisternr,
-        actief: gebruiker.actief === 1 ? 'Ja' : 'Nee',
+        actief: gebruiker.actief === 1 ? "Ja" : "Nee"
       });
     });
   }
@@ -77,36 +75,39 @@ export class ManageGebruikersComponent implements OnInit {
     this.excelModus = !this.excelModus;
   }
   export() {
-    const exportPlugin = this.hotRegisterer.getInstance(this.id).getPlugin('exportFile');
+    const exportPlugin = this.hotRegisterer
+      .getInstance(this.id)
+      .getPlugin("exportFile");
 
-    exportPlugin.downloadFile('csv', {
-      columnDelimiter: ',',
+    exportPlugin.downloadFile("csv", {
+      columnDelimiter: ",",
       columnHeaders: true,
       exportHiddenColumns: true,
       exportHiddenRows: true,
-      fileExtension: 'csv',
-      filename:   'Gebruikers_[YYYY]-[MM]-[DD]',
-      mimeType: 'text/csv',
+      fileExtension: "csv",
+      filename: "Gebruikers_[YYYY]-[MM]-[DD]",
+      mimeType: "text/csv"
     });
   }
 
   onFileChange(evt: any) {
     let exceldata;
     /* wire up file reader */
-    const target: DataTransfer = <DataTransfer>(evt.target);
-    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const target: DataTransfer = <DataTransfer>evt.target;
+    if (target.files.length !== 1) throw new Error("Cannot use multiple files");
     const reader: FileReader = new FileReader();
     reader.onload = (e: any) => {
       /* read workbook */
       const bstr: string = e.target.result;
-      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: "binary" ,cellDates:true});
 
       /* grab first sheet */
       const wsname: string = wb.SheetNames[0];
       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
 
       /* save data */
-      exceldata = (XLSX.utils.sheet_to_json(ws, {header: 1}));
+      exceldata = XLSX.utils.sheet_to_json(ws, { header: 1 });
+      
       this.createObjectOfExcelArrays(exceldata);
     };
     reader.readAsBinaryString(target.files[0]);
@@ -122,20 +123,34 @@ export class ManageGebruikersComponent implements OnInit {
     exceldata.forEach(array => {
       let object = {};
       array.forEach((value, index) => {
-        Object.assign(object, {[headers[index]]: value, rol_id: 4, password: null, eersteAanmelding: true,
-          lunchpakket: false, actief: true, foto: null, geboortedatum: '1983-03-15 00:00:00'});
+        console.log(value);
+        console.log(index);
+        Object.assign(object, {
+          [headers[index]]: value,
+          rol_id: 4,
+          password: null,
+          eersteAanmelding: true,
+          lunchpakket: false,
+          actief: true,
+          foto: null,
+        });
+        object["geboortedatum"] = new Date(object["geboortedatum"]);
+        object["geboortedatum"] = object["geboortedatum"].toLocaleDateString("fr-CA");
+        if(!("opmerking" in object)){
+          object["opmerking"] = "/";
+        }
       });
       objectArray.push(object);
     });
     console.log(objectArray);
-    this.adminService.importGebruikers({gebruikers: objectArray}).subscribe(
+    this.adminService.importGebruikers({ gebruikers: objectArray }).subscribe(
       result => {
-        this.toastr.success('Gebruikers zijn geïmporteerd');
+        this.toastr.success("Gebruikers zijn geïmporteerd");
         this.ngOnInit();
       },
       error => {
         console.log(error);
-        this.toastr.error('Er is iets misgegaan. Verbeter het excelbestand');
+        this.toastr.error("Er is iets misgegaan. Verbeter het excelbestand");
       }
     );
   }
